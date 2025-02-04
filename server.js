cea18c54a8d737eea99cace9580531d986560440
harikrishna87 <krishnahari5048@gmail.com>
2025-02-04 20:35:40 +0530

    Url Shortener

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose")
+const shortUrl = require('./models/shorturl.js')
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+mongoose.connect('mongodb://127.0.0.1:27017/urlshortener', {
+}).then(() => console.log("MongoDB Connected"))
+    .catch(err => console.error("MongoDB Connection Error:", err));
+
+app.set("view engine", "ejs")
+
+app.get('/', async (req, res) => {
+    const shortUrls = await shortUrl.find()
+    res.render('index', { shortUrls: shortUrls })
+})
+
+app.post("/shortUrls", async (req, res) => {
+    await shortUrl.create({ full: req.body.fullUrl })
+    res.redirect('/')
+})
+
+app.get('/:shortUrl', async (req, res) => {
+    const foundUrl = await shortUrl.findOne({ short: req.params.shortUrl })
+    if (shortUrl == null) return res.sendStatus(404)
+
+        foundUrl.clicks++
+        foundUrl.save()
+
+    res.redirect(foundUrl.full)
+})
+
+const port = 4000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
